ef5bedbcaed38f7af04b6a6a9dcf461aa28dfb70
Rostislav Tolushkin <rostislavtoluskinx@gmail.com>
2019-06-01 20:02:24 +0300

    updated digital clock quest logic
    Minor performance improvement

--- DigitalClockV1/main.cpp
+++ DigitalClockV1/main.cpp
@@ -9,7 +9,7 @@ int main() {
     int hours = minutes / 60;
     int shownMinutes = minutes % 60;
 
-    int shownHours = (hours <= 23) ? hours : (hours - (24 * (minutes / (60 * 24))));
+    int shownHours = hours % 24;
     cout << shownHours << " " << shownMinutes;
     return 0;
 }
